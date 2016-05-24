96be5a285a920655b8409bca28f43f6272cbf5a3
bufan <bufanjs@gmail.com>
2016-05-24 09:23:47 +0800

    coderiddle

---
+++ CodeRiddle/demo1.js
@@ -0,0 +1,16 @@
+function foo1(a) {
+    return a + '01';
+}
+
+console.log(foo1(01));
+
+
+function foo2(a) {
+    return a + '010';
+}
+
+console.log(foo2(010));
+
+console.log(0.2 + 0.4);
+
+console.log([4,[3,2]][1][0]);
